407847cfe78b21a17cb6abacab45c0e52516afcc
storm37000 <6331913+storm37000@users.noreply.github.com>
2020-11-20 14:07:48 -0500

    add steam install folder autodetection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Win32;
 
 namespace garrysmod_fixr_uppr
 {
@@ -14,8 +15,9 @@ namespace garrysmod_fixr_uppr
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		private readonly string version = "0.4";
+		private readonly string version = "0.5";
 		private string dir = @"C:\\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod";
+		//private string steamdir = @"C:\\Program Files (x86)\Steam\config";
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -27,7 +29,30 @@ namespace garrysmod_fixr_uppr
 			refresh();
 		}
 
-		private void refresh()
+		private Task getAlternateInstall()
+		{
+			return Task.Run(() => {
+				for (byte i = 1; i < 255; i++)
+				{
+					if (!Directory.Exists(dir))
+					{
+						string search = "\"BaseInstallFolder_" + i + "\"";
+						IEnumerable<String> lines = File.ReadLines(@"C:\\Program Files (x86)\Steam\config\config.vdf").Select(l => new { Line = l, Index = l.IndexOf(search) }).Where(x => x.Index > -1).Select(x => x.Line.Substring(x.Index + search.Length));
+						foreach (string line in lines)
+						{
+							string linedir = line.Trim().Replace("\"", "") + @"\steamapps\common\GarrysMod";
+							if (Directory.Exists(linedir))
+							{
+								dir = linedir + @"\garrysmod";
+								break;
+							}
+						}
+					}
+				}
+			});
+		}
+
+		private async void refresh()
 		{
 			if (Directory.Exists(dir))
 			{
@@ -36,36 +61,30 @@ namespace garrysmod_fixr_uppr
 			}
 			else
 			{
-				if (Directory.Exists(@"C:\\Program Files (x86)\Steam\config"))
+				RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam");
+				string steamdir = key.GetValue("SteamPath").ToString() + @"/config";
+				if (Directory.Exists(steamdir))
 				{
-					string search = "\"BaseInstallFolder_1\"";
-					IEnumerable<String> lines = File.ReadLines(@"C:\\Program Files (x86)\Steam\config\config.vdf").Select(l => new { Line = l, Index = l.IndexOf(search) }).Where(x => x.Index > -1).Select(x => x.Line.Substring(x.Index + search.Length));
-					foreach (var line in lines)
-					{
-						string linedir = line.Trim().Replace("\"", "") + @"\steamapps\common\GarrysMod";
-						if (Directory.Exists(linedir))
-						{
-							dir = linedir + @"\garrysmod";
-							StatusTXT.Foreground = Brushes.ForestGreen;
-							StatusTXT.Text = "garrysmod folder found! (" + dir + ") Check what you want and click apply.";
-							break;
-						}
-					}
+					StatusTXT.Foreground = Brushes.Blue;
+					StatusTXT.Text = "Steam program folder found!  Searching for garrysmod folder...";
+					await getAlternateInstall();
 					if (!Directory.Exists(dir))
 					{
-						StatusTXT.Text = "garrysmod folder NOT found!";
 						StatusTXT.Foreground = Brushes.Red;
-						Applybtn.IsEnabled = false;
+						StatusTXT.Text = "Steam program folder found but garrysmod folder NOT found!";
+					}
+					else
+					{
+						StatusTXT.Foreground = Brushes.ForestGreen;
+						StatusTXT.Text = "garrysmod folder found! (" + dir + ") Check what you want and click apply.";
 					}
 				}
 				else
 				{
 					StatusTXT.Text = "Steam install folder NOT found!";
 					StatusTXT.Foreground = Brushes.Red;
-					Applybtn.IsEnabled = false;
 				}
 			}
-			Applybtn.Click += ApplyBtnClick;
 			check0.IsEnabled = Process.GetProcessesByName("Steam").Length == 0 && (Directory.Exists(dir + @"\..\..\..\workshop\content\4000") || File.Exists(dir + @"\..\..\..\workshop\appworkshop_4000.acf"));
 			check1.IsEnabled = Directory.Exists(dir + @"\cache");
 			check2.IsEnabled = Directory.Exists(dir + @"\downloads");
@@ -73,6 +92,8 @@ namespace garrysmod_fixr_uppr
 			check4.IsEnabled = File.Exists(dir+@"\cl.db") || File.Exists(dir + @"\mn.db");
 			check5.IsEnabled = Directory.Exists(dir + @"\download");
 			check6.IsEnabled = (Directory.Exists(dir + @"\..\bin") && (File.Exists(dir + @"\..\bin\analytics.dll") || File.Exists(dir + @"\..\bin\GameAnalytics.dll"))) || (Directory.Exists(dir + @"\..\bin\win64") && (File.Exists(dir + @"\..\bin\win64\analytics.dll") || File.Exists(dir + @"\..\bin\win64\GameAnalytics.dll")));
+			Applybtn.IsEnabled = check0.IsEnabled || check1.IsEnabled || check2.IsEnabled || check3.IsEnabled || check4.IsEnabled || check5.IsEnabled || check6.IsEnabled;
+			Applybtn.Click += ApplyBtnClick;
 		}
 
 		private void statusmsg(string msg, Brush color)
@@ -136,7 +157,7 @@ namespace garrysmod_fixr_uppr
 			if ((bool)check1.IsChecked)
 			{
 				StatusTXT.Foreground = Brushes.Blue;
-				StatusTXT.Text = "Deleting LUA cache folder...";
+				StatusTXT.Text = "Deleting extracted cache folder...";
 				await deletefolder(dir + @"\cache");
 				check1.IsChecked = false;
 			}
